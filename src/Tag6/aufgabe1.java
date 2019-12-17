a9e901591595b3d8810da5b47cd16e895f517091
mlaurs <markus.laurs@gmx.de>
2019-12-17 15:18:37 +0100

    Tag 6 aber doppelte Orbits werden nur einmal gespeichert also ohne ArrayList als value

--- src/Tag6/aufgabe1.java
+++ src/Tag6/aufgabe1.java
@@ -2,6 +2,7 @@ package Tag6;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 
@@ -9,33 +10,55 @@ public class aufgabe1 {
 
     public static void main(String[] args) throws FileNotFoundException {
         int directAndIndirectOrbits = 0;
-        HashMap<String, String> orbit = new HashMap();
+        HashMap<String, ArrayList<String>> orbit = new HashMap();
         File f = new File("C:\\Users\\mlaurs\\IdeaProjects\\AdventOfCode\\Tag6Werte.txt");
         Scanner sc = new Scanner(f);
 
         while (sc.hasNextLine()) {
             String s = sc.nextLine();
             String[] between = s.split("\\)");
-
-            orbit.put(between[0], between[1]);
+            if (orbit.containsKey(between[0])) {
+                ArrayList<String> a = orbit.get(between[0]);
+                a.add(between[1]);
+                orbit.put(between[0], a);
+            } else {
+                ArrayList<String>b= new ArrayList<>();
+                b.add(between[1]);
+                orbit.put(between[0], b);
+            }
         }
 
         for (String s : orbit.keySet()) {
 
             while (!orbit.get(s).isEmpty()) {
                 directAndIndirectOrbits++;
-                    s = orbit.get(s);
-                    try {
+               ArrayList<String> a = orbit.get(s);
+               if(a.size()==1){
+                   s=a.get(0);
+               }else {
+                   for (int i = 0; i <=a.size() ; i++) {
+                        directAndIndirectOrbits+=a.size();
 
-
-                        if (orbit.get(s).isEmpty()) {
-                            break;
-                        }
-                    }catch (NullPointerException e){
+                   }
+               }
+                try {
+                    if (orbit.get(s).isEmpty()) {
                         break;
                     }
+                } catch (NullPointerException e) {
+                    break;
+                }
             }
         }
         System.out.println(directAndIndirectOrbits);
     }
+
+
+
+
+
+    public static String bestimmenvonValuekeys(){
+
+    }
+
 }
